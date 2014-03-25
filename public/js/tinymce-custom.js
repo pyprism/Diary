4ecdd8c3143bdcef8d2853e526d60276afb9d00b
pyprism <git.pyprism@gmail.com>
2014-03-25 16:47:09 +0600

    working....

---
+++ public/js/tinymce-custom.js
@@ -0,0 +1,12 @@
+/**
+ * Created by prism on 3/25/14.
+ */
+tinymce.init({
+    selector: "textarea",
+    plugins: [
+        "advlist autolink lists link image charmap print preview anchor",
+        "searchreplace visualblocks code fullscreen",
+        "insertdatetime media table contextmenu paste "
+    ],
+    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+});
