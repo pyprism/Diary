f5af49bbf6f82cfbb1c74a52e698502ba06976a5
pyprism <git.pyprism@gmail.com>
2014-05-24 17:15:29 +0600

    shit happend :/

--- app/views/new-post.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('master')
-
-@section('content')
-<p>Now Enter Text,File etc</p>
-{{ Form::open(array('url' => 'new-post')) }}
-    <textarea name="content" style="width:100%"></textarea>
-{{ Form::submit('Save'); }}
-{{ Form::close() }}
-@stop
