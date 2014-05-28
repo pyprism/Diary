092289e3234ad6bfbc44d04fd66379db3dbc6365
pyprism <git.pyprism@gmail.com>
2014-05-28 22:23:32 +0600

    add main layout

---
+++ app/views/layout/main.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Hiren</title>
+    {{ HTML::style('bower/bootstrap/dist/css/bootstrap.css) }}
+    {{ HTML::style('bower/jquery/dist/jquery.js) }}
+    {{ HTML::style('bower/bootstrap/dist/js/bootstrap.js) }}
+</head>
+<body>
+
+</body>
+</html>
