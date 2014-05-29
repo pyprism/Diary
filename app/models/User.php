1ee869f5a5dad588559c09131f141bf0b5375434
pyprism <git.pyprism@gmail.com>
2014-05-29 18:47:14 +0600

    Added some validation rules

--- app/models/User.php
+++ app/models/User.php
@@ -5,6 +5,14 @@ use Illuminate\Auth\Reminders\RemindableInterface;
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
 
+    public static $rules = array(
+        'firstname'=>'required|alpha|min:2',
+        'lastname'=>'required|alpha|min:2',
+        'email'=>'required|email|unique:users',
+        'password'=>'required|alpha_num|between:6,12|confirmed',
+        'password_confirmation'=>'required|alpha_num|between:6,12'
+    );
+
 	/**
 	 * The database table used by the model.
 	 *
