56858953a334e20aeb66d3543e4817ae1480f589
pyprism <git.pyprism@gmail.com>
2014-05-30 19:16:25 +0600

    Added ckeditor js file

---
+++ app/models/Content.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: prism
+ * Date: 5/30/14
+ * Time: 7:09 PM
+ */ 
---
+++ public/editor.js
@@ -0,0 +1,4 @@
+/**
+ * Created by prism on 5/30/14.
+ */
+CKEDITOR.replace( 'editor1' );
