3f6514aee7372ca3a9aeb575c568d8387e6fbc58
pyprism <git.pyprism@gmail.com>
2014-05-30 19:17:08 +0600

    New migration for contents

---
+++ app/database/migrations/2014_05_30_175322_create-content-table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContentTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('content', function($table){
+            $table->foreign('id')->references('id')->on('users');
+            $table->String('Title');
+            $table->longText('text');
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Screma::drop('content');
+	}
+
+}
