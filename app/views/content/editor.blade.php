ff9a1d614771809f90bdd24821a6c54b7d9e985c
pyprism <git.pyprism@gmail.com>
2014-07-02 21:17:54 +0600

    trying new editor

--- app/views/content/editor.blade.php
+++ app/views/content/editor.blade.php
@@ -1,17 +1,58 @@
-@extends('layouts.main')
-@section('content')
-{{ Form::open(array('url' => 'users/editor')) }}
-{{ Form::text('title', 'Enter a title for your post') }}
-{{ Form::textarea('editor1', null, array(
-'id'      => 'editor1',
-'name'    => 'editor1') ) }}
-{{ Form::submit('Click Me Asshole !') }}
-{{ Form::close() }}
-
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Hiren</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    {{ HTML::style('bower/bootstrap/dist/css/bootstrap.min.css') }}
+    {{ HTML::style('bower/Font-Awesome/css/font-awesome.min.css') }}
+    {{ HTML::style('main.css') }}
+    <link rel="shortcut icon" href="{{{ asset('favicon.ico') }}}">
+</head>
+<body>
 <div class="container">
-    @if(Session::has('message'))
-    <p class="alert">{{ Session::get('message') }}</p>
-    @endif
+    <header class="navbar navbar-default navbar-fixed-to" >
+        <a class="navbar-brand" href="/">Hiren</a>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                @if(!Auth::check())
+                <li><a href="{{ url('users/register')}}" class="fa fa-sign-in"> Register</a></li>
+                <li><a href="{{ url('users/login') }}" class="fa fa-heart"> Login</a></li>
+                @else
+                <li><a href="{{ url('users/dashboard')}}" class="fa fa-square-o"> Dashboard</a></li>
+                <li><a href="{{ url('users/editor') }}" class="fa fa-book"> Create New Post</a></li>
+                <li ><a href="https://github.com/pyprism/Diary" class="fa fa-github"> Source Code</a></li>
+                <li><a href="{{ url('users/logout') }}" class="fa fa-sign-out"> Logout</a></li>
+                @endif
+            </ul>
+        </div>
+    </header>
 </div>
 
-@stop
+    @if(Session::has('message'))
+    <p class="alert alert-info">{{ Session::get('message') }}</p>
+    @endif
+<div class="container">
+
+    <div class="raptor-editable" data-id="main-content">
+        <p>
+            Raptor may be integrated into a site many ways.
+            This article aims to cover the simplest integration possible.
+            asdasdasd
+            asd asd ad asd
+             ads asd
+
+        </p>
+    </div>
+</div>
+
+
+
+{{ HTML::script('bower/jquery/dist/jquery.min.js') }}
+{{ HTML::script('bower/bootstrap/dist/js/bootstrap.min.js') }}
+{{ HTML::script('raptor/example/libraries/jquery-ui.js') }}
+{{ HTML::script('raptor/raptor.min.js') }}
+{{ HTML::style('raptor/raptor-front-end.min.css') }}
+{{ HTML::script('editor.js') }}
+</body>
+</html>
