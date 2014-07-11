992e7f94440ee8801658ac88e18714e5a640527f
pyprism <git.pyprism@gmail.com>
2014-07-11 19:49:36 +0600

    complete posts view

--- app/views/content/update.blade.php
+++ app/views/content/update.blade.php
@@ -3,9 +3,9 @@
 <div class="container">
     {{ Form::open() }}
     Title
-    {{ Form::text('title', {{ $post->title }}) }} Tag @if($post->tag){{ Form::text('tag', {{ $post->tag }}) }}@else{{ Form::text('tag') }@endif
-    {{ Form::textarea('content', {{ $post->text }}) }}
-    {{ Form::submit('Save', array('class' => 'btn')) }}
+    {{ Form::text('title', $post[0]->title) }} Tag {{ Form::text('tag', $post[0]->tag) }}
+    {{ Form::textarea('content', $post[0]->text) }}
+    {{ Form::submit('Update', array('class' => 'btn')) }}
     {{ Form::close() }}
 </div>
 @stop
