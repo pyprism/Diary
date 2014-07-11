9d982a31f9690d17383a9ee0fe2d760ef669661b
pyprism <git.pyprism@gmail.com>
2014-07-11 19:49:59 +0600

    fix an error

--- app/views/content/post.blade.php
+++ app/views/content/post.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.main')
 @section('content')
 {{ $post[0] }}
-<a href="{{ url('user/update'). '/' . $id }}"><button type="button" class="btn btn-primary">Edit</button></a>
+<a href="{{ url('users/update'). '/' . $id }}"><button type="button" class="btn btn-primary">Edit</button></a>
 @stop
