97e5e85998d4483c688729e8d62fffb1238d54c5
pyprism <git.pyprism@gmail.com>
2014-07-11 19:48:36 +0600

    add js files

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -11,29 +11,28 @@
 </head>
 <body>
 <div class="container">
-<header class="navbar navbar-default navbar-fixed-top" >
-    <a class="navbar-brand" href="/">Hiren</a>
-    <div class="collapse navbar-collapse">
-        <ul class="nav navbar-nav">
-            @if(!Auth::check())
-            <li><a href="{{ url('users/register')}}" class="fa fa-sign-in"> Register</a></li>
-            <li><a href="{{ url('users/login') }}" class="fa fa-heart"> Login</a></li>
-            @else
-            <li><a href="{{ url('users/dashboard')}}" class="fa fa-square-o"> Dashboard</a></li>
-            <li><a href="{{ url('users/editor') }}" class="fa fa-book"> Create New Post</a></li>
-            <li ><a href="https://github.com/pyprism/Diary" class="fa fa-github"> Source Code</a></li>
-            <li><a href="{{ url('users/logout') }}" class="fa fa-sign-out"> Logout</a></li>
-            <li><a href="{{ url('users/logout') }}" class="fa fa-user navbar-right"> Hi, {{ username }}!</a></li>
-            @endif
-        </ul>
-    </div>
-</header>
+    <header class="navbar navbar-default navbar-fixed-to" >
+        <a class="navbar-brand" href="/">Hiren</a>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                @if(!Auth::check())
+                <li><a href="{{ url('users/register')}}" class="fa fa-sign-in"> Register</a></li>
+                <li><a href="{{ url('users/login') }}" class="fa fa-heart"> Login</a></li>
+                @else
+                <li><a href="{{ url('users/dashboard')}}" class="fa fa-square-o"> Dashboard</a></li>
+                <li><a href="{{ url('users/editor') }}" class="fa fa-book"> Create New Post</a></li>
+                <li ><a href="https://github.com/pyprism/Diary" class="fa fa-github"> Source Code</a></li>
+                <li><a href="{{ url('users/logout') }}" class="fa fa-sign-out"> Logout</a></li>
+                @endif
+            </ul>
+        </div>
+    </header>
 </div>
-{{-- <div class="container"> --}}
+ <div class="container">
     @if(Session::has('message'))
     <p class="alert alert-info">{{ Session::get('message') }}</p>
     @endif
-{{-- </div> --}}
+</div>
 <div class="container" >
     {{-- {{ $content }} --}}
     @yield('content')
@@ -43,7 +42,7 @@
 
 {{ HTML::script('bower/jquery/dist/jquery.min.js') }}
 {{ HTML::script('bower/bootstrap/dist/js/bootstrap.min.js') }}
-{{ HTML::script('bower/ckeditor/ckeditor.js') }}
+{{ HTML::script('bower/tinymce/tinymce.min.js') }}
 {{ HTML::script('editor.js') }}
 </body>
 </html>
