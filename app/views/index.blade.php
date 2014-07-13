4fa66a89386f0535c8faddf0dfdf3f8e74ba7212
pyprism <git.pyprism@gmail.com>
2014-07-13 18:18:35 +0600

    css clean up

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -7,35 +7,7 @@
     {{ HTML::style('bower/bootstrap/dist/css/bootstrap.min.css') }}
     {{ HTML::style('bower/Font-Awesome/css/font-awesome.min.css') }}
     {{ HTML::style('main.css') }}
-	<style>
-		@import url(//fonts.googleapis.com/css?family=Lato:700);
 
-		body {
-			margin:0;
-			font-family:'Lato', sans-serif;
-			text-align:center;
-			color: #999;
-		}
-
-		.welcome {
-			width: 300px;
-			height: 200px;
-			position: absolute;
-			left: 50%;
-			top: 50%;
-			margin-left: -150px;
-			margin-top: -100px;
-		}
-
-		a, a:visited {
-			text-decoration:none;
-		}
-
-		h1 {
-			font-size: 32px;
-			margin: 16px 0 0 0;
-		}
-	</style>
 </head>
 <body>
 	<div class="welcome">
--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     {{ HTML::style('bower/bootstrap/dist/css/bootstrap.min.css') }}
     {{ HTML::style('bower/Font-Awesome/css/font-awesome.min.css') }}
+    {{ HTML::style('bower/animate.css/animate.min.css') }}
     {{ HTML::style('main.css') }}
     <link rel="shortcut icon" href="{{{ asset('favicon.ico') }}}">
 </head>
