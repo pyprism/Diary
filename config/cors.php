e4ad160b09f83fa1e7822758b1816448b173f526
Ashutosh Das <git.pyprism@gmail.com>
2015-04-11 21:12:52 +0600

    added cors conf

---
+++ config/cors.php
@@ -0,0 +1,47 @@
+<?php
+
+return array(
+
+    /*
+     |--------------------------------------------------------------------------
+     | Laravel CORS Defaults
+     |--------------------------------------------------------------------------
+     |
+     | The defaults are the default values applied to all the paths that match,
+     | unless overridden in a specific URL configuration.
+     | If you want them to apply to everything, you must define a path with *.
+     |
+     | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*') 
+     | to accept any value, the allowed methods however have to be explicitly listed.
+     |
+     */
+    'defaults' => array(
+        'supportsCredentials' => false,
+        'allowedOrigins' => array('*'),
+        'allowedHeaders' => array('*'),
+        'allowedMethods' => array('*'),
+        'exposedHeaders' => array(),
+        'maxAge' => 0,
+        'hosts' => array('*'),
+    ),
+
+    'paths' => array(
+        '*' => array(
+            'allowedOrigins' => array('*'),
+            'allowedHeaders' => array('*'),
+            'allowedMethods' => array('*'),
+            'maxAge' => 3600,
+            'hosts' => array('*'),
+        ),
+      //  '*' => array(
+     //       'allowedOrigins' => array('*'),
+     //       'allowedHeaders' => array('Content-Type'),
+     //     //  'allowedMethods' => array('POST', 'PUT', 'GET', 'DELETE'),
+      //      'allowedMethods' => array('*'),
+      //      'maxAge' => 3600,
+      //     // 'hosts' => array('api.*'),
+     //       'hosts' => array('*'),
+     //   ),
+    ),
+
+);
