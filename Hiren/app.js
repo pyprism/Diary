0476420d5f6cc538000c20f6d94a2b2be17cdcb4
Ashutosh Das <git.pyprism@gmail.com>
2015-04-17 22:01:27 +0600

    added basic markup

--- Hiren/app.js
+++ Hiren/app.js
@@ -1,4 +1,28 @@
 /**
  * Created by prism on 4/16/15.
  */
-angular.module('Hiren', )
+angular.module('Hiren', ['ngResource', 'ngRoute', 'satellizer'] )
+    .config(function($authProvider, $routeProvider, $locationProvider) {
+       $routeProvider
+           .when('/', {
+               templateUrl: 'partials/login.html',
+               controller: 'LoginCtrl'
+           })
+           .when('/logout', {
+               templateUrl: 'partials/logout.html',
+               controller: 'LogoutCtrl'
+           })
+           .when('/posts', {
+               templateUrl: 'partials/allPosts.html',
+               controller: 'AllPostCtrl'
+           })
+           .when('/posts/:postId', {
+               templateUrl: 'partials/post.html',
+               controller: 'PostCtrl'
+           })
+           .when('/posts/new', {
+               templateUrl: 'partials/new.html',
+               controller: 'NewCtrl'
+           });
+        $locationProvider.html5Mode(true);
+    });
