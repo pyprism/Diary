dd1a2d58b3924310bb2a30ae5d34ccd3f6721c84
Ashutosh Das <git.pyprism@gmail.com>
2015-04-18 21:09:15 +0600

    finally login working :smile:

--- Hiren/controllers/login.js
+++ Hiren/controllers/login.js
@@ -3,5 +3,18 @@
  */
 angular.module('Hiren')
     .controller('LoginCtrl', function($scope, $auth) {
+        $scope.login = function() {
+            $auth.login({ email: $scope.email, password: $scope.password})
+                .then(function(response) {
+                    console.log('login' + response.data);
+                });
+        };
+
+        $scope.authenticate = function(provider) {
+          $auth.authenticate(provider)
+              .then(function(response) {
+                  console.log('authenticated' + response.data);
+              });
+        };
 
     });
