941c1cf65fa2b02e2f42f8c2e89e0b8befc5b7b0
Ashutosh Das <git.pyprism@gmail.com>
2016-04-05 23:26:35 +0600

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+/**
+ * Created by prism on 4/5/16.
+ */
+var path = require("path");
+var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
+
+module.exports = {
+    entry: [
+        './static/app/app.jsx'
+    ],
+    output : {
+        path: __dirname,
+        filename: './static/js/bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            }
+           // { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel?transform-es2015-arrow-functions'}
+        ]
+    },
+
+    plugins: process.env.NODE_ENV === 'production' ? [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin()
+    ] : [new BundleTracker({filename: './webpack-stats.json'})],
+};
