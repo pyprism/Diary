f069e230602e10bf5ba61fab7f7c14492764068a
Ashutosh Das <git.pyprism@gmail.com>
2016-04-07 22:55:58 +0600

    added model test

--- diary/tests.py
+++ diary/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from .models import Tag, Notes, Diary
+from django.utils import timezone
 # Create your tests here.
+
+
+class ModelTest(TestCase):
+
+    def setUp(self):
+        current_date_time = timezone.now()
+        tag = Tag.objects.create(name="Test tag")
+        Notes.objects.create(tag=tag, content="Uhalala Uhalla :D ", date=current_date_time)
+        Diary.objects.create(tag=tag, title="Hello title", content="test content", date=current_date_time)
+
+    def test_tag_model(self):
+        tag_item = Tag.objects.all()
+        self.assertEqual(tag_item.count(), 1)
