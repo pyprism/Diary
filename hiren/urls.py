c0d8e526e3c1981081f2b859fb71c18be6949430
Ashutosh Das <git.pyprism@gmail.com>
2016-04-07 22:56:14 +0600

    added api url

--- hiren/urls.py
+++ hiren/urls.py
@@ -13,13 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework import routers
+from diary import views
+
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'tags', views.TagViewSet)
+router.register(r'notes', views.NotesViewset)
+router.register(r'diary', views.DiaryViewset)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^docs/', include('rest_framework_swagger.urls')),
     url(r'^', TemplateView.as_view(template_name='index.html')),
 ]
