e38bd3cfdc82e3b398f4a17321879f8c265e8d61
Ashutosh Das <git.pyprism@gmail.com>
2016-04-07 20:16:08 +0600

    updated model

--- diary/models.py
+++ diary/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+
+
 class Diary(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=500)
     content = models.TextField()
     date = models.DateTimeField()
+    tag = models.ForeignKey('Tag')
 
 
 class Notes(models.Model):
     content = models.TextField()
     date = models.DateTimeField()
+    tag = models.ForeignKey('Tag')
