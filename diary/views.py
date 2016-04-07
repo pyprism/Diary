e2e5bd3b5e11ea0a543d46c426868aceaf36b504
Ashutosh Das <git.pyprism@gmail.com>
2016-04-07 22:56:42 +0600

    added view

--- diary/views.py
+++ diary/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from .serializers import UserSerializer, TagSerializer, NotesSerializer, DiarySerializer
+from .models import Tag, Diary, Notes
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    """
+        API endpoint that allows tags to be created, viewed ,edited and deleted.
+    """
+    authentication_classes = (SessionAuthentication, BasicAuthentication, JSONWebTokenAuthentication)
+    permission_classes = (IsAuthenticated,)
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class NotesViewset(viewsets.ModelViewSet):
+    """
+        API endpoint that allows notes to be created, viewed ,edited and deleted.
+    """
+    authentication_classes = (SessionAuthentication, BasicAuthentication, JSONWebTokenAuthentication)
+    permission_classes = (IsAuthenticated,)
+    queryset = Notes.objects.all()
+    serializer_class = NotesSerializer
+
+
+class DiaryViewset(viewsets.ModelViewSet):
+    """
+        API endpoint that allows diary to be created, viewed ,edited and deleted.
+    """
+    authentication_classes = (SessionAuthentication, BasicAuthentication, JSONWebTokenAuthentication)
+    permission_classes = (IsAuthenticated,)
+    queryset = Diary.objects.all()
+    serializer_class = DiarySerializer
