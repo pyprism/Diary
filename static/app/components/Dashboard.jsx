6b6cf04c94e3a796b1f2bdd0ffa5672f0dcde184
Ashutosh Das <git.pyprism@gmail.com>
2016-05-14 22:08:04 +0600

    updated component

--- static/app/components/Dashboard.jsx
+++ static/app/components/Dashboard.jsx
@@ -1,4 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Helmet from "react-helmet";
 
-export default class Dashboard extends React.Component {}
+export default class Dashboard extends React.Component {
+    render() {
+        return (
+            <div>
+                <Helmet
+                    title="Diary: Dashboard"
+                    link={[
+                    {"rel": "stylesheet", "href": "/css/bootstrap.min.css"},
+                    {"rel": "stylesheet", "href": "/css/simple-sidebar.css"},
+                    {"rel": "stylesheet", "href": "/css/font-awesome.min.css"}
+                ]}
+                />
+            </div>
+        )
+    }
+}
