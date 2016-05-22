97090253c05705594802c53faad71308489d108c
Ashutosh Das <git.pyprism@gmail.com>
2016-05-22 23:51:33 +0600

    added sidebar

--- static/app/components/Sidebar.jsx
+++ static/app/components/Sidebar.jsx
@@ -5,7 +5,33 @@ import {Link} from 'react-router';
 export default class Sidebar extends React.Component {
     render() {
         return (
-            <div></div>
+            <div>
+
+                <div id="sidebar-wrapper">
+            <ul className="sidebar-nav nav-pills nav-stacked" id="menu">
+
+                <li class="active">
+                    <a href="simple_sidebar_menu.html#"><span className="fa-stack fa-lg pull-left"><i className="fa fa-dashboard fa-stack-1x "></i></span> Dashboard</a>
+                       <ul className="nav-pills nav-stacked bunny" >
+                        <li><a href="simple_sidebar_menu.html#">link1</a></li>
+                        <li><a href="simple_sidebar_menu.html#">link2</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="simple_sidebar_menu.html#"><span className="fa-stack fa-lg pull-left"><i className="fa fa-flag fa-stack-1x "></i></span> Shortcut</a>
+                    <ul className="nav-pills nav-stacked bunny">
+                        <li><a href="simple_sidebar_menu.html#"><span className="fa-stack fa-lg pull-left"><i className="fa fa-flag fa-stack-1x "></i></span>link1</a></li>
+                        <li><a href="simple_sidebar_menu.html#"><span className="fa-stack fa-lg pull-left"><i className="fa fa-flag fa-stack-1x "></i></span>link2</a></li>
+
+                    </ul>
+                </li>
+                <li>
+                    <a href="simple_sidebar_menu.html#"><span className="fa-stack fa-lg pull-left"><i className="fa fa-cloud-download fa-stack-1x "></i></span>Overview</a>
+                </li>
+            </ul>
+        </div>
+
+            </div>
         )
     }
 }
