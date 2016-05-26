a01afbd8a381553edfe659f0d4a7892cbcf2d008
Ashutosh Das <git.pyprism@gmail.com>
2016-05-26 20:58:13 +0600

    rearranged theme inheritance

--- static/app/components/Tags.jsx
+++ static/app/components/Tags.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import Sidebar from './Sidebar.jsx';
+import Bunny from "./Bunny.jsx";
+
 
 export default class Tags extends React.Component {
     render() {
         return (
-             <div id="page-content-wrapper">
-            <div className="container-fluid xyz">
-                <div className="row">
-                    <div className="col-lg-12">
-                        <h1>Simple Sidebar With Bootstrap 3 by <a href="http://seegatesite.com/create-simple-cool-sidebar-menu-with-bootstrap-3/" >Seegatesite.com</a></h1>
-                        <p>This sidebar is adopted from start bootstrap simple sidebar startboostrap.com, which I modified slightly to be more cool. For tutorials and how to create it , you can read from my site here <a href="http://seegatesite.com/create-simple-cool-sidebar-menu-with-bootstrap-3/">create cool simple sidebar menu with boostrap 3</a></p>
+            <div>
+                <Bunny />
+                 <div id="wrapper">
+                     <Sidebar />
+                <div id="page-content-wrapper">
+                    <div className="container-fluid xyz">
+                        <div className="row">
+                            <div className="col-lg-12">
+                                <h1>Tag <a href="http://seegatesite.com/create-simple-cool-sidebar-menu-with-bootstrap-3/" >Seegatesite.com</a></h1>
+                                <p>This sidebar is adopted from start bootstrap simple sidebar startboostrap.com, which I modified slightly to be more cool. For tutorials and how to create it , you can read from my site here <a href="http://seegatesite.com/create-simple-cool-sidebar-menu-with-bootstrap-3/">create cool simple sidebar menu with boostrap 3</a></p>
+                            </div>
+                        </div>
                     </div>
                 </div>
+                     </div>
             </div>
-                 <Sidebar />
-        </div>
         )
     }
 }
