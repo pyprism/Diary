1f64bfcbe0736b049d8748c1d34cbe4eac7e475d
Ashutosh Das <git.pyprism@gmail.com>
2016-05-31 22:31:36 +0600

    changed field

--- diary/models.py
+++ diary/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
 
 
 class Diary(models.Model):
