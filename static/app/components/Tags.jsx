13720154ec98aafe849d88f085d93a6240024ade
Ashutosh Das <git.pyprism@gmail.com>
2016-05-31 22:33:41 +0600

    finished new tag component

--- static/app/components/Tags.jsx
+++ static/app/components/Tags.jsx
@@ -1,26 +1,40 @@
 import React from 'react';
 import Sidebar from './Sidebar.jsx';
 import Bunny from "./Bunny.jsx";
+import Tag_Ajax from "../store/Tag_Store.jsx";
+import ReactDOM from 'react-dom';
 
 
 export default class Tags extends React.Component {
+
+    newTag(e){
+        e.preventDefault();
+        Tag_Ajax.addTag(ReactDOM.findDOMNode(this.refs.new_tag).value);
+        ReactDOM.findDOMNode(this.refs.new_tag).value = "";  // clear field 
+    }
+
     render() {
         return (
             <div>
                 <Bunny />
-                 <div id="wrapper">
-                     <Sidebar />
-                <div id="page-content-wrapper">
-                    <div className="container-fluid xyz">
-                        <div className="row">
-                            <div className="col-lg-12">
-                                <h1>Tag <a href="http://seegatesite.com/create-simple-cool-sidebar-menu-with-bootstrap-3/" >Seegatesite.com</a></h1>
-                                <p>This sidebar is adopted from start bootstrap simple sidebar startboostrap.com, which I modified slightly to be more cool. For tutorials and how to create it , you can read from my site here <a href="http://seegatesite.com/create-simple-cool-sidebar-menu-with-bootstrap-3/">create cool simple sidebar menu with boostrap 3</a></p>
+                <div id="wrapper">
+                    <Sidebar />
+                    <div id="page-content-wrapper">
+                        <div className="container-fluid xyz">
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    <form onSubmit={this.newTag.bind(this)}>
+                                        <div className="form-group">
+                                            <label>Create New Tag</label>
+                                            <input type="text" className="form-control" required ref="new_tag" placeholder="New Tag Name" />
+                                        </div>
+                                        <button type="submit" className="btn btn-default"><i className="fa fa-bookmark"> Save</i></button>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                     </div>
             </div>
         )
     }
