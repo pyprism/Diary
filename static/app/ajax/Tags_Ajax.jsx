82a56a61860476f336ea3301b920730baee1aa8f
Ashutosh Das <git.pyprism@gmail.com>
2016-06-02 22:33:30 +0600

    .

--- static/app/ajax/Tags_Ajax.jsx
+++ static/app/ajax/Tags_Ajax.jsx
@@ -2,5 +2,34 @@ import axios from 'axios';
 
 
 export default class Tags_Ajax {
-    
+    static checkToken() {
+        try {
+            sessionStorage.getItem('token')
+        } catch (e) {
+            if (e instanceof ReferenceError) {
+                window.location.href = "/";
+            }
+        }
+    }
+
+    static addTag(tag) {
+
+        Tags_Ajax.checkToken();
+
+        axios({
+            method: 'post',
+            url: '/api/tags/',
+            headers: {'Authorization': 'JWT ' + sessionStorage.getItem('token')},
+            data: {
+                'username': username,
+                'password': password
+            }
+        }).then(function (response) {
+            sessionStorage.setItem('token', response.data['token']);
+            window.location.href = "/dashboard";
+            })
+            .catch(function (response) {
+                console.error(response);
+            });
+    }
 }
