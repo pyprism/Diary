65cce9a1d61468dd6fac833e4e8f08efc576c5f6
Ashutosh Das <git.pyprism@gmail.com>
2016-06-02 22:29:34 +0600

    added secretkey component

--- static/app/components/Dashboard.jsx
+++ static/app/components/Dashboard.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import { observable, computed } from "mobx";
 import { observer } from "mobx-react";
 import Sidebar from "./Sidebar.jsx";
-import Page from "./Page.jsx";
 import Bunny from "./Bunny.jsx";
+import GetSecretKey from "./GetSecretKey.jsx";
+import SetSecretKey from "./SetSecretKey.jsx";
 
 
 @observer export default class Dashboard extends React.Component {
@@ -16,13 +17,45 @@ import Bunny from "./Bunny.jsx";
         };
     }
 
+    bunny() {
+        var secretkey;
+        try {
+            if(sessionStorage.getItem('key')) {
+                return secretkey = < GetSecretKey key={sessionStorage.getItem('key')} />;
+            } else {
+               return  secretkey = < SetSecretKey />;
+            }
+        } catch (e) {
+            if (e instanceof ReferenceError) {
+                return secretkey = < SetSecretKey />;
+            }
+        }
+    }
+
     render() {
+
+        var secretkey;
+            if(sessionStorage.getItem('key')) {
+                secretkey = < GetSecretKey key={sessionStorage.getItem('key')} />;
+            } else {
+               secretkey = < SetSecretKey />;
+            }
+
+
         return (
             <div>
                 <Bunny />
                 <div id="wrapper">
                     <Sidebar />
-                    <Page />
+                    <div id="page-content-wrapper">
+                        <div className="container-fluid xyz">
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    { secretkey }
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
