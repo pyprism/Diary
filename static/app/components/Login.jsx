a2bccee9972505bcd788e7d192cc13d680b65bcb
Ashutosh Das <git.pyprism@gmail.com>
2016-06-05 21:39:43 +0600

    refactored some code

--- static/app/components/Login.jsx
+++ static/app/components/Login.jsx
@@ -1,45 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Helmet from "react-helmet";
-import Auth from "../ajax/Auth.jsx";
+import axios from 'axios';
+import { browserHistory } from 'react-router';
 
 var $script = require("scriptjs");
 
 $script("../../static/bower/sweetalert/dist/sweetalert.min.js");
 
 export default class Login extends React.Component {
-        render () {
+
+    login(e) {
+        e.preventDefault();
+        axios({
+            method: 'post',
+            url: '/api-token-auth/',
+            data: {
+                'username': ReactDOM.findDOMNode(this.refs.username).value,
+                'password': ReactDOM.findDOMNode(this.refs.password).value
+            }
+        }).then(function (response) {
+            if (response.data['token']) {
+                sessionStorage.setItem('token', response.data['token']);
+                browserHistory.push('/dashboard');
+            }
+        })
+            .catch(function (response) {
+                sweetAlert("Oops!", response.data.non_field_errors[0], "error");
+            });
+    }
+
+    render () {
         return <div className="login">
             <Helmet
                 title="Hiren-Diary: Login"
                 link={[
-                    {"rel": "stylesheet", "href": "static/css/login.css"},
-                    {"rel": "icon", "href": "static/favicon.ico"},
+                    {"rel": "stylesheet", "href": "/static/css/login.css"},
+                    {"rel": "icon", "href": "/static/favicon.ico"},
                     {"rel": "stylesheet", "type": "text/css", "href": "/static/bower/sweetalert/dist/sweetalert.css"}
 
                 ]}
             />
-                <form onSubmit={this.login.bind(this)}>
+            <form onSubmit={this.login.bind(this)}>
 
-                    <label>
-                        <input type="text" required ref="username"/>
-                        <div className="label-text">User name</div>
-                    </label>
-                    <label>
-                        <input type="password" required ref="password"/>
-                        <div className="label-text">Password</div>
-                    </label>
-                    <button className="button" type="submit" > Login </button>
-                </form>
+                <label>
+                    <input type="text" required ref="username"/>
+                    <div className="label-text">User name</div>
+                </label>
+                <label>
+                    <input type="password" required ref="password"/>
+                    <div className="label-text">Password</div>
+                </label>
+                <button className="button" type="submit" > Login </button>
+            </form>
 
-            </div>
+        </div>
 
 
     }
-
-     login(e) {
-        e.preventDefault();
-        Auth.login(ReactDOM.findDOMNode(this.refs.username).value, ReactDOM.findDOMNode(this.refs.password).value);
-        }
 
 }
