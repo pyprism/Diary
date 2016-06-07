6c8cb50eced62b7df2cb11e8eff77787f184e564
Ashutosh Das <git.pyprism@gmail.com>
2016-06-07 21:55:57 +0600

    refactored routing and template inheritance

--- static/app/app.jsx
+++ static/app/app.jsx
@@ -7,14 +7,29 @@ import {Router, Route, browserHistory} from 'react-router';
 import Login from './components/Login.jsx';
 import Dashboard from './components/Dashboard.jsx';
 import Tags from './components/Tags.jsx';
+import Main from './components/Main.jsx';
 
 
 
+/*
 ReactDOM.render(
     <Router history={browserHistory} >
         <Route path="/" component={Login}> </Route>
         <Route path="/dashboard" component={Dashboard}> </Route>
         <Route path="/dashboard/tags" component={Tags}> </Route>
+    </Router>,
+    document.getElementById('app')
+);
+*/
+
+ReactDOM.render(
+    <Router history={browserHistory} >
+        <Route path="/" component={Login}> </Route>
+        <Route path="/dashboard" component={Main}>
+            <Route path="stats" component={Dashboard} />
+            <Route path="tags" component={Tags} />
+        </Route>
+
     </Router>,
     document.getElementById('app')
 );
