d4ff8b21fa922a1251bac0a90483a01ee4a52778
Ashutosh Das <git.pyprism@gmail.com>
2016-06-11 22:47:38 +0600

    added  token check

--- static/app/app.jsx
+++ static/app/app.jsx
@@ -8,24 +8,33 @@ import Login from './components/Login.jsx';
 import Dashboard from './components/Dashboard.jsx';
 import Tags from './components/Tags.jsx';
 import Main from './components/Main.jsx';
+import axios from 'axios';
 
 
 
-/*
+function authRequired(nextState, replace) {
+    let token = sessionStorage.getItem('token');
+    if (token) {
+        axios({
+            method: 'post',
+            url: '/api-token-verify/',
+            data: {
+                'token': token
+            }
+        }).catch(function(response) {
+            console.log(response);
+            replace('/');
+        });
+    } else {
+        replace('/');
+    }
+
+}
+
 ReactDOM.render(
     <Router history={browserHistory} >
         <Route path="/" component={Login}> </Route>
-        <Route path="/dashboard" component={Dashboard}> </Route>
-        <Route path="/dashboard/tags" component={Tags}> </Route>
-    </Router>,
-    document.getElementById('app')
-);
-*/
-
-ReactDOM.render(
-    <Router history={browserHistory} >
-        <Route path="/" component={Login}> </Route>
-        <Route path="/dashboard" component={Main}>
+        <Route path="/dashboard" onEnter={authRequired} component={Main}>
             <Route path="stats" component={Dashboard} />
             <Route path="tags" component={Tags} />
         </Route>
