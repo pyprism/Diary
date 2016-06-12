c4fea051fac3f692578e246f7312b968d5cd3cc1
Ashutosh Das <git.pyprism@gmail.com>
2016-06-12 14:25:28 +0600

    added new view

--- diary/views.py
+++ diary/views.py
@@ -3,11 +3,12 @@ from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from .serializers import UserSerializer, TagSerializer, NotesSerializer, DiarySerializer
-from .models import Tag, Diary, Notes
+from .serializers import UserSerializer, TagSerializer, NotesSerializer, DiarySerializer, SecretSerializer
+from .models import Tag, Diary, Notes, Secret
 from django.core import serializers
 from rest_framework.decorators import detail_route, list_route
 from rest_framework.response import Response
+from rest_framework import status
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -58,3 +59,34 @@ class DiaryViewset(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
     queryset = Diary.objects.all()
     serializer_class = DiarySerializer
+
+
+class SecretViewset(viewsets.ModelViewSet):
+    """
+        API endpoint that allows secret key  to be created, viewed ,edited and deleted.
+    """
+    authentication_classes = (SessionAuthentication, BasicAuthentication, JSONWebTokenAuthentication)
+    permission_classes = (IsAuthenticated,)
+    queryset = Secret.objects.all()
+    serializer_class = SecretSerializer
+
+    def create(self, request, *args, **kwargs):
+        """
+        Check if secret key already created
+        """
+        count = Secret.objects.all().count()
+        if count == 1:
+            content = {'error': 'key already exits'}
+            return Response(content, status.HTTP_403_FORBIDDEN)
+        else:
+            instance = self.get_object()
+            Secret.objects.create(key=instance)
+            serializer = self.get_serializer(instance)
+            return Response(serializer.data, status.HTTP_201_CREATED)
+
+    def destroy(self, request, pk=None):
+        bunny = {'error': 'method not supported :/'}
+        return Response(bunny, status.HTTP_403_FORBIDDEN)
+
+
+
