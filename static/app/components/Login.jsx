f158074349179d00478d622a4a9029a951004aac
Ashutosh Das <git.pyprism@gmail.com>
2016-06-13 19:07:46 +0600

    changed route

--- static/app/components/Login.jsx
+++ static/app/components/Login.jsx
@@ -22,7 +22,7 @@ export default class Login extends React.Component {
         }).then(function (response) {
             if (response.data['token']) {
                 sessionStorage.setItem('token', response.data['token']);
-                browserHistory.push('/dashboard/stats');
+                browserHistory.push('/dashboard/secret/');
             }
         })
             .catch(function (response) {
