cc38990cd536fa79c6ded1156bb8be7a8d115591
Ashutosh Das <git.pyprism@gmail.com>
2016-06-13 20:03:09 +0600

    added new component

---
+++ static/app/components/Secret.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import Crypt from '../utils/Crypt.jsx';
+import { browserHistory } from 'react-router';
+
+
+export default class Secret extends React.Component {
+    secret(e){
+        e.preventDefault();
+        let random = Array(8+1).join((Math.random().toString(36)+'00000000000000000').slice(2, 18)).slice(0, 8);
+        //sessionStorage.setItem('key', ReactDOM.findDOMNode(this.refs.secret).value);
+        //ReactDOM.findDOMNode(this.refs.secret).value = "";  // clear field
+        axios({
+            method: 'get',
+            url: '/api/secret/1/',
+            headers: {'Authorization': "JWT " + sessionStorage.getItem('token')}
+        }).then(function (response) {
+            console.log(response);
+            console.log('hitti');
+        }).catch(function (response) {
+            if(response.status == 404){
+                // axios({
+                //     method: 'post',
+                //     url: '/api/secret/',
+                //     data: {'key': Crypt.encrypt(ReactDOM.findDOMNode(this.refs.secret).value, random)},
+                //     headers: {'Authorization': "JWT " + sessionStorage.getItem('token')}
+                // }).then(function(response) {
+                //     console.table(response);
+                // })
+                console.log('hit');
+                console.log(Crypt.encrypt(ReactDOM.findDOMNode(this.refs.secret).value, random));
+                console.log('hit');
+            }else if (response.status == 403) {
+                browserHistory.push('/');
+                sweetAlert("Oops!", 'Token Expired! Login in again.', "error");
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.secret.bind(this)}>
+                    <div className="form-group">
+                        <label>Enter Your Encryption Key</label>
+                        <input type="text" className="form-control" required ref="secret" placeholder="Encryption Key" />
+                    </div>
+                    <button type="submit" className="btn btn-default"><i className="fa fa-bookmark"> Save</i></button>
+                </form>
+            </div>
+        )
+    }
+}
