90ee6c60162859d352081924c4550b52c6f8e490
Ashutosh Das <git.pyprism@gmail.com>
2016-06-16 20:36:50 +0600

    added new component

---
+++ static/app/components/Posts.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import { browserHistory } from 'react-router';
+
+
+export default class Posts extends React.Component {
+
+    render() {
+        return(
+            <h1>
+                Posts
+            </h1>
+        )
+    }
+}
