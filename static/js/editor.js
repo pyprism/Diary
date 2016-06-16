f86b34744a88c78bdba715471c4acd747af7679e
Ashutosh Das <git.pyprism@gmail.com>
2016-06-16 20:36:31 +0600

    added new js

---
+++ static/js/editor.js
@@ -0,0 +1,10 @@
+/**
+ * Created by prism on 6/16/16.
+ */
+/*
+$(document).ready(function() {
+  $('textarea').summernote({
+      height: 300,
+  });
+    $('#datetimepicker1').datetimepicker();
+});*/
