c08d82376585d1fca31192576727d83c49745ffc
Ashutosh Das <git.pyprism@gmail.com>
2017-01-07 17:20:17 +0600

    wip

--- diary/views.py
+++ diary/views.py
@@ -8,9 +8,11 @@ from .models import Diary, Notes
 from rest_framework.decorators import list_route
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.mixins import ListModelMixin
+from django.core import serializers
 
 
-class NotesViewset(viewsets.ModelViewSet):
+class NotesViewset(viewsets.ModelViewSet, ListModelMixin):
     """
         API endpoint that allows notes to be created, viewed ,edited and deleted.
     """
@@ -18,6 +20,17 @@ class NotesViewset(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
     queryset = Notes.objects.all()
     serializer_class = NotesSerializer
+
+    def perform_create(self, serializer):
+        instance = serializer.save()
+        if 'tags' in self.request.data:
+            instance.tag.set(*self.request.data['tags'])
+
+    def list(self, request, *args, **kwargs):
+        notes = Notes.objects.all()
+        data = serializers.serialize('json', notes)
+        content = {'hiren': data}
+        return Response(content)
 
 
 class DiaryViewset(viewsets.ModelViewSet):
