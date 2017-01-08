5f05a3a652824eb5b63eafd75d56a6f7e41f2eeb
Ashutosh Das <git.pyprism@gmail.com>
2017-01-08 21:21:16 +0600

    Added login component

---
+++ bunny/components/Login.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Helmet from "react-helmet";
+import axios from 'axios';
+import { browserHistory } from 'react-router';
+
+export default class Login extends React.Component {
+
+    login(e) {
+        e.preventDefault();
+        axios({
+            method: 'post',
+            url: '/api-token-auth/',
+            data: {
+                'username': ReactDOM.findDOMNode(this.refs.username).value,
+                'password': ReactDOM.findDOMNode(this.refs.password).value
+            }
+        }).then(function (response) {
+            if (response.data['token']) {
+                sessionStorage.setItem('token', response.data['token']);
+                browserHistory.push('/secret/');
+            }
+        })
+            .catch(function (response) {
+                sweetAlert("Oops!", 'Username/Password is not correct', "error");
+            });
+    }
+    
+    render () {
+        return <span>
+            <Helmet
+                title="Hiren-Diary: Login"
+                link={[
+                    {"rel": "stylesheet", "href": "/static/css/login.css"},
+                    {"rel": "icon", "href": "/static/favicon.ico"},
+                    {"rel": "stylesheet", "type": "text/css", "href": "/static/css/sweetalert.css"}
+
+                ]}
+            />
+
+            <form id="loginform"  onSubmit={this.login.bind(this)} >
+                <input type="text" required className="input" placeholder="Username" ref="username" />
+                <input type="password" required className="input" placeholder="Password" ref="password" />
+                <input type="submit" className="loginbutton" value="SIGN IN" />
+            </form>
+
+        </span>
+
+
+    }
+
+}
