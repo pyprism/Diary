32bccce4d284da9667eecda9beeef2b50a72b584
Ashutosh Das <git.pyprism@gmail.com>
2017-01-10 13:08:26 +0600

    added default value

--- diary/models.py
+++ diary/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from taggit.managers import TaggableManager
+from django.utils import timezone
 
 
 class Diary(models.Model):
     title = models.CharField(max_length=500)
     content = models.TextField()
     iv = models.CharField(max_length=500)
-    date = models.DateTimeField()
+    date = models.DateTimeField(default=timezone.now)
     tag = TaggableManager()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -15,7 +16,7 @@ class Diary(models.Model):
 class Notes(models.Model):
     content = models.TextField()
     iv = models.CharField(max_length=500)
-    date = models.DateTimeField()
+    date = models.DateTimeField(default=timezone.now)
     tag = TaggableManager()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
