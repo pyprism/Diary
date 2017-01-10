9b3f2842f8ac301ca9927051ce4db982ed62799f
Ashutosh Das <git.pyprism@gmail.com>
2017-01-10 18:33:11 +0600

    added post component

---
+++ bunny/components/Post.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Helmet from "react-helmet";
+import axios from 'axios';
+import { browserHistory } from 'react-router';
+
+
+export default class Post extends React.Component {
+    render() {
+        return (
+            <div>
+                Single Post
+            </div>
+        )
+    }
+
+}
