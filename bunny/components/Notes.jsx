3bea70acd704f8d194bf5cad7ad57301a5a3ca58
Ashutosh Das <git.pyprism@gmail.com>
2017-01-10 18:33:35 +0600

    added notes component

---
+++ bunny/components/Notes.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Helmet from "react-helmet";
+import axios from 'axios';
+import { browserHistory } from 'react-router';
+
+
+export default class Notes extends React.Component {
+    render() {
+        return (
+            <div>
+                Notes
+            </div>
+        )
+    }
+
+}
