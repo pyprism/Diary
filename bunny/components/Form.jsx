58c8ff171d8d6c11091f96f6abfb5b6de48abeb4
Ashutosh Das <git.pyprism@gmail.com>
2017-01-12 12:27:17 +0600

    refactored form design

--- bunny/components/Form.jsx
+++ bunny/components/Form.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Helmet from "react-helmet";
 import axios from 'axios';
 import { browserHistory } from 'react-router';
-import {Crypt} from '../utils/Crypt';
+import Crypt from '../utils/Crypt';
 
 
 export default class Form extends React.Component {
@@ -38,7 +38,7 @@ export default class Form extends React.Component {
             sweetAlert('Error', 'Check console!', 'error');
         })
     }
-    
+
     postType(e) {
         if(e.target.value == 'note') {
             $('#title').hide();
@@ -48,6 +48,15 @@ export default class Form extends React.Component {
             $('#title').show();
             return true;
         }
+    }
+
+    str2ab(str) {
+        var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
+        var bufView = new Uint16Array(buf);
+        for (var i=0, strLen=str.length; i<strLen; i++) {
+            bufView[i] = str.charCodeAt(i);
+        }
+        return buf;
     }
 
     componentDidMount() {
@@ -87,70 +96,74 @@ export default class Form extends React.Component {
             })
         })();
 
-        var cipher = forge.cipher.createCipher('AES-CBC', this.stringToBytes('s'));
-         cipher.start({iv: "sasdrsdffcdfasdfercf34rc3c3c"});
+        /*var salt = forge.random.getBytesSync(128);
+         console.info(forge.util.bytesToHex(salt));
+         var _iv = forge.random.getBytesSync(16);
+         var key = forge.pkcs5.pbkdf2('password', salt, 64000, 16);
+         var cipher = forge.cipher.createCipher('AES-CBC', key);
+         cipher.start({iv: _iv});
          cipher.update(forge.util.createBuffer("Sasasasa"));
          cipher.finish();
          var encrypted = cipher.output;
-         console.log(encrypted);
-         console.log(encrypted.toHex());
+         //console.log(encrypted);
+         //console.log(encrypted.toHex());
+         //console.log(forge.util.hexToBytes(encrypted.toHex()));
+
+         var decipher = forge.cipher.createDecipher('AES-CBC', key);
+         decipher.start({iv: _iv});
+         decipher.update(encrypted);
+         decipher.finish();
+         // outputs decrypted hex
+         //console.log(decipher.output.data);*/
     }
 
     render() {
 
         return (
-            <div className="container">
-                <div className="row">
-                    <Helmet
-                        title="Hiren-Diary: Form"
-                    />
-                    <div className="col-sm-6 col-sm-offset-2">
-                        <form className="form-horizontal pad-bg" onSubmit={this.form.bind(this)}>
-                            <h4 className="text-center">Create New Post</h4>
-                            <div className="form-group">
-                                <label className="control-label col-sm-3">Post Type</label>
-                                <div className="col-sm-9">
-                                    <select className="form-control input-lg" onChange={this.postType.bind(this)} required>
-                                        <option value="diary">Diary</option>
-                                        <option value="note">Note</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div className="form-group" id="title">
-                                <label className="control-label col-sm-3">Title</label>
-                                <div className="col-sm-9">
-                                    <input type="text" ref="title" required placeholder="Title of the post" className="form-control input-lg" />
-                                </div>
-                            </div>
-                            <div className="form-group" id="title">
-                                <label className="control-label col-sm-3">Content</label>
-                                <div className="col-sm-9">
-                                    <textarea id="summernote" type="text" ref="content" required placeholder="Title of the post" className="form-control input-lg" />
-                                </div>
-                            </div>
-                            <div className="form-group">
-                                <label className="control-label col-sm-3">Date</label>
-                                <div className='input-group date' id='datetimepicker'>
-                                    <input type='text' ref="date" className="form-control input-lg" />
+            <div >
+                <form className="form-horizontal">
+                    <div className="form-group">
+                        <label className="control-label col-sm-2">Post Type</label>
+                        <div className="col-sm-10">
+                            <select className="form-control " onChange={this.postType.bind(this)} required>
+                                <option value="diary">Diary</option>
+                                <option value="note">Note</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="form-group" id="title">
+                        <label className="control-label col-sm-2" >Title</label>
+                        <div className="col-sm-10">
+                            <input type="text" className="form-control" ref="title" placeholder="Post Title"/>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <label className="control-label col-sm-2" >Content</label>
+                        <div className="col-sm-10">
+                            <textarea id="summernote" ref="content" className="form-control" placeholder="Enter Content"/>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <label className="control-label col-sm-2">Date</label>
+                        <div className='input-group date' id='datetimepicker'>
+                            <input type='text' ref="date" className="form-control" />
                                     <span className="input-group-addon">
                                         <span className="glyphicon glyphicon-calendar" />
                                     </span>
-                                </div>
-                            </div>
-                            <div className="form-group" >
-                                <label className="control-label col-sm-3">Tags</label>
-                                <div className="col-sm-9">
-                                    <input type="text" ref="tag" id="tags" required className="form-control input-lg" />
-                                </div>
-                            </div>
-                            <div className="form-group">
-                                <div className="col-sm-offset-3 col-sm-9">
-                                    <button type="submit" className="btn btn-default btn-lg"><i className="fa fa-save"></i> Save</button>
-                                </div>
-                            </div>
-                        </form>
+                        </div>
                     </div>
-                </div>
+                    <div className="form-group" >
+                        <label className="control-label col-sm-2">Tags</label>
+                        <div className="col-sm-10">
+                            <input type="text" ref="tag" id="tags" required className="form-control input-lg" />
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <div className="col-sm-offset-2 col-sm-10">
+                            <button type="submit" className="btn btn-default"><i className="fa fa-save" /> Save</button>
+                        </div>
+                    </div>
+                </form>
             </div>
         )
     }
