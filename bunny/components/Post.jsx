fddac16c5acf74cf571090f8c1cf1f371ad5bc9c
Ashutosh Das <git.pyprism@gmail.com>
2017-01-20 09:41:01 +0600

    wip

--- bunny/components/Post.jsx
+++ bunny/components/Post.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Helmet from "react-helmet";
 import axios from 'axios';
+import { toJS } from "mobx";
 import { browserHistory } from 'react-router';
+import { observer } from "mobx-react";
 
 
 export default class Post extends React.Component {
+    
+    componentDidMount() {
+        console.log(this.props.route.posts.findPostById(this.props.params.id));
+    }
+    
     render() {
         return (
             <div>
                 Single Post
+                {this.props.params.id}
             </div>
         )
     }
