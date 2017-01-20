7ea60705b2b66374c54f99a2be3a176b41c63cee
Ashutosh Das <git.pyprism@gmail.com>
2017-01-20 12:02:55 +0600

    wip

--- bunny/components/Post.jsx
+++ bunny/components/Post.jsx
@@ -7,17 +7,28 @@ import { browserHistory } from 'react-router';
 import { observer } from "mobx-react";
 
 
+@observer
 export default class Post extends React.Component {
     
     componentDidMount() {
-        console.log(this.props.route.posts.findPostById(this.props.params.id));
+        this.props.route.posts.pageId = this.props.params.id;
+        this.props.route.posts.findPostById();
     }
     
     render() {
         return (
             <div>
-                Single Post
-                {this.props.params.id}
+                <Helmet
+                        title="Hiren-Diary: Post"
+                        link = {[
+                    {"rel": "stylesheet", "type": "text/css", "href": "https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic"},
+                    {"rel": "stylesheet", "type": "text/css", "href": "https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800"},
+                    {"rel": "stylesheet", "type": "text/css", "href": "/static/css/hiren.css"}
+            ]}
+                    />
+                <article>
+                    <h1>{this.props.route.posts.post['title']}</h1>
+                </article>
             </div>
         )
     }
