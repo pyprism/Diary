3a12121e45cfe94a3b543cc01d134339888d6e06
Ashutosh Das <git.pyprism@gmail.com>
2017-01-21 20:58:28 +0600

    wip

--- bunny/components/Post.jsx
+++ bunny/components/Post.jsx
@@ -5,30 +5,73 @@ import axios from 'axios';
 import { toJS } from "mobx";
 import { browserHistory } from 'react-router';
 import { observer } from "mobx-react";
+import {Link} from 'react-router';
 
 
 @observer
 export default class Post extends React.Component {
-    
+
     componentDidMount() {
         this.props.route.posts.pageId = this.props.params.id;
         this.props.route.posts.findPostById();
+        console.log(toJS(this.props.route.posts.post['tag']));
     }
-    
+
+    delete() {
+        axios({
+            method: 'delete',
+            url: '/api/diary/' + this.props.route.posts.pageId + '/',
+            headers: {
+                'Authorization': "JWT " + sessionStorage.getItem('token')
+            }
+        })
+    }
+
+    tags() {
+        var bunny = [];
+        (this.props.route.posts.post['tag']).map(function (data) {
+            bunny.push(
+                <button className="btn btn-default" role="button" >
+                    #{data}
+                </button>
+            )
+        });
+        return (
+            <div> {bunny} <Link className="btn btn-info"
+                                 to={'/dashboard/posts/' + this.props.route.posts.pageId + '/edit/'}
+                                 role="button">Edit</Link>
+                <button className="btn btn-danger"  role="button" onClick={this.delete()}>Delete</button>
+            </div>
+        );
+    }
+
     render() {
         return (
-            <div>
+            <div className="posts">
                 <Helmet
-                        title="Hiren-Diary: Post"
-                        link = {[
+                    title="Hiren-Diary: Post"
+                    link = {[
                     {"rel": "stylesheet", "type": "text/css", "href": "https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic"},
                     {"rel": "stylesheet", "type": "text/css", "href": "https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800"},
                     {"rel": "stylesheet", "type": "text/css", "href": "/static/css/hiren.css"}
             ]}
-                    />
-                <article>
-                    <h1>{this.props.route.posts.post['title']}</h1>
-                </article>
+                />
+
+                <div className="container">
+                    <div className="row">
+                        <h2 className="post-title">
+                            {this.props.route.posts.post['title']}
+                        </h2>
+                        <hr/>
+                        <article>
+                            <div dangerouslySetInnerHTML={{__html: this.props.route.posts.post['content']}} />
+                        </article>
+                        <hr/>
+                        <p className="post-meta">Posted on {this.props.route.posts.post['date']}</p>
+                        {this.tags()}
+                    </div>
+                </div>
+
             </div>
         )
     }
