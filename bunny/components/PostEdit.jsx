b9c6554abf317b406a4bea00ca9962215cb112c7
Ashutosh Das <git.pyprism@gmail.com>
2017-01-23 21:34:35 +0600

    updated form

--- bunny/components/PostEdit.jsx
+++ bunny/components/PostEdit.jsx
@@ -4,80 +4,48 @@ import Helmet from "react-helmet";
 import axios from 'axios';
 import { browserHistory } from 'react-router';
 import Crypt from '../utils/Crypt';
+import { toJS } from "mobx";
+
 
 
 export default class PostEdit extends React.Component {
 
     form(e) {
         /***
-         * Handle post save
+         * Handle post update
          */
         e.preventDefault();
 
-        // detect post type
-        if(ReactDOM.findDOMNode(this.refs.postType).value == 'note')
-            var _url = '/api/notes/';
-        else if(ReactDOM.findDOMNode(this.refs.postType).value == 'diary')
-            var _url = '/api/diary/';
+        let  random = this.props.route.posts.post['iv'],
+            key = this.props.route.posts.post['key'],
+            _id = this.props.route.posts.pageId,
+            salt = forge.util.bytesToHex(this.props.route.posts.post['salt']);
 
-        // key, salt generation
-        let  random = forge.random.getBytesSync(16),
-            _salt = forge.random.getBytesSync(128),
-            key = forge.pkcs5.pbkdf2(sessionStorage.getItem('key'), _salt, 100, 16);
-
-        // input tag string manipulation for django-taggit format
-        var tagStr = ReactDOM.findDOMNode(this.refs.tag).value;
-        var _tag =[];
-        var len = tagStr.length;
-        var hiren;
-        var x = "";
-        for( hiren=0; hiren<len; hiren++) {
-            if(tagStr[hiren] != ";"){
-                x = x + tagStr[hiren];
-                if( hiren + 1 == len)
-                    _tag.push(x);
-            }
-            else{
-                _tag.push(x);
-                x = "";
-            }
-        }
-
-        // now the final part. clap clap ....  :D
         axios({
-            method: 'post',
-            url: _url,
+            method: 'put',
+            url: '/api/diary/' + _id + '/',
             data: {
-                'tag' : _tag,
+                'tag' : this.props.route.posts.post['tag'],
                 'title': Crypt.encrypt(ReactDOM.findDOMNode(this.refs.title).value, key, random),
                 'content': Crypt.encrypt(ReactDOM.findDOMNode(this.refs.content).value, key, random),
                 'iv': forge.util.bytesToHex(random),
-                'salt': forge.util.bytesToHex(_salt),
+                'salt': salt,
                 'date': ReactDOM.findDOMNode(this.refs.date).value
             },
             headers: {
                 'Authorization': "JWT " + sessionStorage.getItem('token')
             }
         }).then(function (response) {
-            console.log(response);
-            if(response.statusText === "Created") {
-                sweetAlert("Saved", "Saved Successfully", "success");
+            if(response.statusText === "OK") {
+                sweetAlert("Updated", "Updated Successfully", "success");
                 browserHistory.push('/dashboard/posts/');
             }
         }).catch(function (error) {
             if(error.statusText === 'Forbidden') {
                 sweetAlert("Oops!", 'Token Expired, Log Out Plz !', "error");
             }
-            console.error(error);
             sweetAlert('Error', error.statusText, 'error');
         })
-    }
-
-    postType(e) {
-        if(e.target.value == 'note')
-            $('#title').hide();
-        else if (e.target.value == 'diary')
-            $('#title').show();
     }
 
     componentDidMount() {
@@ -89,53 +57,6 @@ export default class PostEdit extends React.Component {
         $('#datetimepicker').datetimepicker({  // initializer for datetimepicker
             format: 'YYYY-MM-D H:mm:ss'
         });
-
-        (function () { // function for selectize
-            axios.get('/api/tags/', {
-                headers: {'Authorization': "JWT " + sessionStorage.getItem('token')}
-            }).then(function (response) {
-                let nisha = [];
-                response.data.map(function (hiren) {
-                    let bunny = {'value': '', 'text': ''};
-                    bunny['value'] = hiren.name;
-                    bunny['text'] = hiren.name;
-                    nisha.push(bunny);
-                });
-                $('#tags').selectize({
-                    delimiter: ';',
-                    persist: false,
-                    options: nisha,
-                    create: function(input) {
-                        return {
-                            value: input,
-                            text: input
-                        }
-                    }
-                })
-            }).catch(function (error) {
-                console.error(error);
-            })
-        })();
-
-        /*var salt = forge.random.getBytesSync(128);
-         console.info(forge.util.bytesToHex(salt));
-         var _iv = forge.random.getBytesSync(16);
-         var key = forge.pkcs5.pbkdf2('password', salt, 64000, 16);
-         var cipher = forge.cipher.createCipher('AES-CBC', key);
-         cipher.start({iv: _iv});
-         cipher.update(forge.util.createBuffer("Sasasasa"));
-         cipher.finish();
-         var encrypted = cipher.output;
-         //console.log(encrypted);
-         //console.log(encrypted.toHex());
-         //console.log(forge.util.hexToBytes(encrypted.toHex()));
-
-         var decipher = forge.cipher.createDecipher('AES-CBC', key);
-         decipher.start({iv: _iv});
-         decipher.update(encrypted);
-         decipher.finish();
-         // outputs decrypted hex
-         //console.log(decipher.output.data);*/
     }
 
     render() {
@@ -144,50 +65,35 @@ export default class PostEdit extends React.Component {
             <div >
                 <div className="row">
                     <Helmet
-                        title="Hiren-Diary: Form"
+                        title="Hiren-Diary: Edit Post"
                     />
                 </div>
 
                 <form className="form-horizontal" onSubmit={this.form.bind(this)}>
-                    <div className="form-group">
-                        <label className="control-label col-sm-2">Post Type</label>
-                        <div className="col-sm-10">
-                            <select className="form-control " ref="postType" onChange={this.postType.bind(this)} required>
-                                <option value="diary">Diary</option>
-                                <option value="note">Note</option>
-                            </select>
-                        </div>
-                    </div>
                     <div className="form-group" id="title">
                         <label className="control-label col-sm-2" >Title</label>
                         <div className="col-sm-10">
-                            <input type="text" className="form-control" ref="title" placeholder="Post Title"/>
+                            <input type="text" className="form-control" defaultValue={this.props.route.posts.post['title']} ref="title" placeholder="Post Title"/>
                         </div>
                     </div>
                     <div className="form-group">
                         <label className="control-label col-sm-2" >Content</label>
                         <div className="col-sm-10">
-                            <textarea id="summernote" ref="content" className="form-control" placeholder="Enter Content"/>
+                            <textarea id="summernote" defaultValue={this.props.route.posts.post['content']} ref="content" className="form-control" placeholder="Enter Content"/>
                         </div>
                     </div>
                     <div className="form-group">
                         <label className="control-label col-sm-2">Date</label>
                         <div className='input-group date' id='datetimepicker'>
-                            <input type='text' ref="date" className="form-control" />
+                            <input type='text' defaultValue={this.props.route.posts.post['rawDate']} ref="date" className="form-control" />
                                     <span className="input-group-addon">
                                         <span className="glyphicon glyphicon-calendar" />
                                     </span>
                         </div>
                     </div>
-                    <div className="form-group" >
-                        <label className="control-label col-sm-2">Tags</label>
-                        <div className="col-sm-10">
-                            <input type="text" ref="tag" id="tags" required className="form-control input-lg" />
-                        </div>
-                    </div>
                     <div className="form-group">
                         <div className="col-sm-offset-2 col-sm-10">
-                            <button type="submit" className="btn btn-default"><i className="fa fa-save" /> Save</button>
+                            <button type="submit" className="btn btn-default"><i className="fa fa-save" /> Update</button>
                         </div>
                     </div>
                 </form>
