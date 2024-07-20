dc12ed3d8a0b6576ac4d53c629feac88c45ffe28
Ashutosh Das <git.pyprism@gmail.com>
2024-07-20 22:13:53 +0600

    added new model method

--- diary/managers.py
+++ diary/managers.py
@@ -5,6 +5,9 @@ class TagManager(models.Manager):
     def create_tag(self, user, tag):
         return self.create(user=user, tag=tag)
 
+    def get_tag(self, user, tag):
+        return self.filter(user=user, name=tag).first()
+
 
 class DiaryManager(models.Manager):
     def create_diary(self, user, diary, post_type, tag):
