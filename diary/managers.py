47d4362b0433c121a3fe2e054368e5631f4e0a6c
Ashutosh Das <git.pyprism@gmail.com>
2024-08-10 21:57:27 +0600

    added new model manager

--- diary/managers.py
+++ diary/managers.py
@@ -8,6 +8,11 @@ class TagManager(models.Manager):
     def get_tag(self, user, tag):
         return self.filter(user=user, name=tag).first()
 
+    def get_all_tags(self, user=None):
+        if user:
+            return self.filter(user=user).order_by('name')
+        return self.order_by('name')
+
 
 class DiaryManager(models.Manager):
     def create_diary(self, user, diary, post_type, tag):
