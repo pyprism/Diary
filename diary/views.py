fafcbdeedd154ce19822f8cec8de1a6857fb8a8e
Ashutosh Das <git.pyprism@gmail.com>
2024-08-10 21:57:43 +0600

    Added tag viewset

--- diary/views.py
+++ diary/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from diary.models import Tag
+from diary.serializers import TagSerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.get_all_tags()
+    permission_classes = [IsAuthenticated]
+    serializer_class = TagSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = Tag.objects.get_all_tags(user)
+        return queryset
+
+
+
