a35009a582cdbad29e6749e346673d091c87a7dc
Ashutosh Das <git.pyprism@gmail.com>
2024-08-12 04:19:55 +0600

    added unique validation

--- diary/serializers.py
+++ diary/serializers.py
@@ -16,6 +16,16 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = ('id', 'name', 'created_at', 'updated_at')
 
+    def validate(self, data):
+        user = self.context['request'].user
+        name = data.get('name')
+
+        # Check if the tag with the same name already exists for this user
+        if Tag.objects.filter(user=user, name=name).exists():
+            raise serializers.ValidationError("A tag with this name already exists for the user.")
+
+        return data
+
     def create(self, validated_data):
         user = self.context['request'].user
         validated_data['user'] = user
