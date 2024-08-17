5813400c1b2b6900fd508128c489422761a45001
Ashutosh Das <git.pyprism@gmail.com>
2024-08-17 23:39:18 +0600

    added diary api

--- diary/urls.py
+++ diary/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register('tags', views.TagViewSet, basename='tags')
+router.register('diaries', views.DiaryViewSet, basename='diaries')
 
 urlpatterns = [
     re_path(r'^v1/', include(router.urls)),
